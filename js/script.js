358827e6a9b2134fd2e0b0cd18f9c19d39dd31fa
AnGraizzZ <pekpak@mail.ru>
2022-01-11 23:25:17 +0300

    commit 1.1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Вкладки
 catalog.onclick = function() {
     var acatalog = document.getElementById("catalog");
     var afavor = document.getElementById("favor");
@@ -20,4 +21,24 @@ catalog.onclick = function() {
     var afavor = document.getElementById("content-favor");
     acatalog.classList.add("no-active-content");
     afavor.classList.remove("no-active-content");
+  };
+
+  //Список пользователей
+  window.onload = function(){ 
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        //console.log(typeof xhr.responseText);
+        //console.log(xhr.responseText);
+      }
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        const res = JSON.parse(xhr.responseText);
+        console.log(res);
+      };
+    };
+    xhr.open('GET', 'https://json.medrating.org/users/', true);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.send(null);
+
+
   };
