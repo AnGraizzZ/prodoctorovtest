9f9a55addd3e508789370737395d12b5bbd01252
AnGraizzZ <pekpak@mail.ru>
2022-01-15 23:32:29 +0300

    commit 1.2

--- js/script.js
+++ js/script.js
@@ -25,20 +25,57 @@ catalog.onclick = function() {
 
   //Список пользователей
   window.onload = function(){ 
+    treeHTML=document.getElementById("treeHTML-catalog");
     const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        //console.log(typeof xhr.responseText);
-        //console.log(xhr.responseText);
-      }
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        const res = JSON.parse(xhr.responseText);
-        console.log(res);
-      };
-    };
     xhr.open('GET', 'https://json.medrating.org/users/', true);
     xhr.setRequestHeader('Accept', 'application/json');
     xhr.send(null);
-
-
+   // setTimeout(1000);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status==200) {
+        const res = JSON.parse(xhr.responseText);
+        treeHTML.innerHTML='';
+        for (var key in res)
+        {
+          console.log(key, res[key].name);
+          var ContentHTML = ' \
+          <div >\
+          <p class="tree-parent" >'+res[key].name+'</p>\
+          <details id="user'+res[key].id+'"><summary class="circle"  onclick="album('+res[key].id+')" ><div class="horizontal"></div><div class="vertical"></div></summary>\
+          <div class="lbox"><div class="loading"></div></div>\
+          </details>\
+          </div>\
+          ';
+          treeHTML.innerHTML+= ContentHTML;
+        }
+      };
+    };
+  };
+//Альбомы пользователей
+  function album(user){
+   // userHTML=document.getElementById("treeHTML-catalog");
+    userHTML=document.getElementById("user"+user);
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', ' https://json.medrating.org/albums?userId='+user, true);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.send(null); 
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status==200) {
+        const res = JSON.parse(xhr.responseText);
+        userHTML.innerHTML='<summary class="circle"><div class="horizontal"></div><div class="vertical"></div></summary>';
+        for (var key in res)
+        {
+         // console.log(key, res[key].title);
+          var ContentHTML = ' \
+          <div >\
+          <p class="tree-parent" >'+res[key].title+'</p>\
+          <details id="user'+res[key].userId+res[key].id+'"><summary class="circle"  onclick="photo('+res[key].id+')" ><div class="horizontal"></div><div class="vertical"></div></summary>\
+          <div class="lbox"><div class="loading"></div></div>\
+          </details>\
+          </div>\
+          ';
+          userHTML.innerHTML+= ContentHTML;
+        }
+      };
+    };
   };
