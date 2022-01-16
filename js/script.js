58fcebae9f3cd3cbf016afbab8ccf1b099574378
AnGraizzZ <pekpak@mail.ru>
2022-01-16 03:14:30 +0300

    commit 1.3

--- js/script.js
+++ js/script.js
@@ -37,9 +37,9 @@ catalog.onclick = function() {
         treeHTML.innerHTML='';
         for (var key in res)
         {
-          console.log(key, res[key].name);
+          //console.log(key, res[key].name);
           var ContentHTML = ' \
-          <div >\
+          <div class="class-user">\
           <p class="tree-parent" >'+res[key].name+'</p>\
           <details id="user'+res[key].id+'"><summary class="circle"  onclick="album('+res[key].id+')" ><div class="horizontal"></div><div class="vertical"></div></summary>\
           <div class="lbox"><div class="loading"></div></div>\
@@ -51,6 +51,7 @@ catalog.onclick = function() {
       };
     };
   };
+  
 //Альбомы пользователей
   function album(user){
    // userHTML=document.getElementById("treeHTML-catalog");
@@ -67,9 +68,9 @@ catalog.onclick = function() {
         {
          // console.log(key, res[key].title);
           var ContentHTML = ' \
-          <div >\
+          <div class="class-album">\
           <p class="tree-parent" >'+res[key].title+'</p>\
-          <details id="user'+res[key].userId+res[key].id+'"><summary class="circle"  onclick="photo('+res[key].id+')" ><div class="horizontal"></div><div class="vertical"></div></summary>\
+          <details id="photo'+res[key].id+'"><summary class="circle"  onclick="photo('+res[key].id+')" ><div class="horizontal"></div><div class="vertical"></div></summary>\
           <div class="lbox"><div class="loading"></div></div>\
           </details>\
           </div>\
@@ -79,3 +80,44 @@ catalog.onclick = function() {
       };
     };
   };
+
+  //фото пользователей
+  function photo(album){
+    // userHTML=document.getElementById("treeHTML-catalog");
+     albumHTML=document.getElementById("photo"+album);
+     const xhr = new XMLHttpRequest();
+     xhr.open('GET', ' https://json.medrating.org/photos?albumId='+album, true);
+     xhr.setRequestHeader('Accept', 'application/json');
+     xhr.send(null); 
+     xhr.onreadystatechange = function () {
+       if (xhr.readyState === XMLHttpRequest.DONE && xhr.status==200) {
+        console.log(typeof xhr.responseText);
+        console.log(xhr.responseText);
+         const res = JSON.parse(xhr.responseText);
+         albumHTML.innerHTML='<summary class="circle"><div class="horizontal"></div><div class="vertical"></div></summary><div class="photoalbum">';
+         for (var key in res)
+         {
+          console.log(key, res[key].title);
+           var ContentHTML = ' \
+                                    <div class="photo">\
+                                    <div class="overlay" id="urlphoto'+res[key].albumId+res[key].id+'">\
+                                        <div class="overlay_container">\
+                                            <a href="#close">\
+                                                <img src="'+res[key].url+'"/>\
+                                            </a>\
+                                        </div>\
+                                    </div>\
+                                    <div class="star"></div>\
+                                        <a href="#urlphoto'+res[key].albumId+res[key].id+'">\
+                                            <img  id="usephoto'+res[key].albumId+res[key].id+'" src="'+res[key].thumbnailUrl+'" alt="'+res[key].title+'" title="'+res[key].title+'">\
+                                        </a>\
+                                    </div>\
+           ';
+           albumHTML.innerHTML+= ContentHTML;
+         }
+         albumHTML.innerHTML+='</div>';
+       };
+     };
+   };
+
+   
